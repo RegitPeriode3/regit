12b3546200b9b648fa7bb3755bfa0c030b8c236b
emily <emily.jacob.nl@gmail.com>
2023-02-15 12:41:12 +0100

    front end main
    elements: header, footer, sidebar etc en login scherm begin

---
+++ elements/sidebar.php
@@ -0,0 +1,54 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+  <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+      <a class="nav-link collapsed">
+        <i class="bi bi-clock"></i>
+        <span>uren registratie</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed">
+        <i class="bi bi-person"></i>
+        <span>facturatie</span>
+      </a>
+    </li>
+
+    <hr>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed">
+        <i class="bi bi-person"></i>
+        <span>klant beheer</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed">
+        <i class="bi bi-person"></i>
+        <span>gebruiker beheer</span>
+      </a>
+    </li>
+
+    <hr>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed">
+      <i class="bi bi-gear"></i>
+        <span>Instellingen</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed">
+        <i class="bi bi-box-arrow-in-right"></i>
+        <span>Uitloggen</span>
+      </a>
+    </li>
+
+  </ul>
+
+</aside><!-- End Sidebar-->
