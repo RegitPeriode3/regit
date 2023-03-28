e652220afc2aef26cca05d1bb5f15b075b304346
Stef Berben <Stef.Berben@Student.GildeOpleidingen.nl>
2023-03-28 09:41:08 +0200

    echo username

--- elements/header.php
+++ elements/header.php
@@ -27,7 +27,7 @@ $username = $_SESSION['username'];
 
       <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
         <img src="../assets/img/profile-img.png" alt="Profile" class="rounded-circle">
-        <span class="d-none d-md-block dropdown-toggle ps-2"><?php //echo $username ?></span>
+        <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo $username ?></span>
       </a><!-- End Profile Iamge Icon -->
     </li><!-- End Profile Nav -->
 
