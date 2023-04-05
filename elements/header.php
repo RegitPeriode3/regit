55a985f440bba85f935b37788ffa5e8fad0e1254
emily <emily.jacob.nl@gmail.com>
2023-04-05 10:10:20 +0200

    selectie tussen admin / medewerker sidebar

--- elements/header.php
+++ elements/header.php
@@ -14,8 +14,8 @@ $username = $_SESSION['username'];
 <div class="d-flex align-items-center justify-content-between">
   <a href="index.html" class="logo d-flex align-items-center">
     <!-- <img src="assets/img/logo.png" alt=""> -->
-    <span class="d-none d-lg-block">Regit -</span>
-    <span class="d-none d-lg-block header-company-name">bedrijfsnaam</span>
+    <span class="d-none d-lg-block">Regit</span>
+    <!-- <span class="d-none d-lg-block header-company-name">bedrijfsnaam</span> -->
   </a>
   <i class="bi bi-list toggle-sidebar-btn"></i>
 </div><!-- End Logo -->
--- elements/sidebar.php
+++ elements/sidebar.php
@@ -1,60 +1,78 @@
-
 <!-- ======= Sidebar ======= -->
 <aside id="sidebar" class="sidebar">
-    <script src="../assets/js/CompanyManagement.js"></script>
+  <script src="../assets/js/CompanyManagement.js"></script>
   <ul class="sidebar-nav" id="sidebar-nav">
 
-    <li class="nav-item">
-      <a href="#" class="nav-link collapsed" data-target="HourRegistration">
-        <i class="bi bi-clock"></i>
-        <span>Uren registratie</span>
-      </a>
-    </li>
+    <?php
+    $Id = $_SESSION['clearence_id'];
+    echo $Id;
 
-    <!--not visible on mobile-->
-    <div class="invisi-nav">
+    if ($Id == 1) { //normal user
+    ?>
+
       <li class="nav-item">
-        <a href="#" class="nav-link collapsed" data-target="Billing">
-          <i class="bi bi-archive"></i>
-          <span>Facturatie</span>
+        <a href="#" class="nav-link collapsed" data-target="HourRegistration">
+          <i class="bi bi-clock"></i>
+          <span>Uren registratie</span>
         </a>
       </li>
-
       <hr>
-
-    <li class="nav-item">
-      <a href="#" class="nav-link collapsed" id="CompanyManagementNav" data-target="ClientManagement">
-        <i class="bi bi-file-earmark-person"></i>
-        <span onclick="clearForms()">Klantbeheer</span>
-
-      </a>
-    </li>
-
-    <li class="nav-item">
-      <a href="#" class="nav-link collapsed" id="UserManagementNav" data-target="UserManagement">
-        <i class="bi bi-person-gear"></i>
-        <span onclick="clearForms()">Gebruikerbeheer</span>
-      </a>
-    </li>
-
-      <hr>
-
-    <li class="nav-item">
-      <a href="#" class="nav-link collapsed" data-target="Settings">
-        <i class="bi bi-gear"></i>
-        <span >Instellingen</span>
-
-      </a>
-    </li>
-
       <a href="http://localhost/regit/pages/logout.php" class="nav-link collapsed">
-    <li class="nav-item">
-
-        <i class="bi bi-box-arrow-in-right"></i>
-        <span>Uitloggen</span>
-
-    </li>
+        <li class="nav-item">
+          <i class="bi bi-box-arrow-in-right"></i>
+          <span>Uitloggen</span>
+        </li>
       </a>
+
+    <?php
+    } else { //admin
+    ?>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link collapsed" data-target="HourRegistration">
+          <i class="bi bi-clock"></i>
+          <span>Uren registratie</span>
+        </a>
+      </li>
+      <!--not visible on mobile-->
+      <div class="invisi-nav">
+        <li class="nav-item">
+          <a href="#" class="nav-link collapsed" data-target="Billing">
+            <i class="bi bi-archive"></i>
+            <span>Facturatie</span>
+          </a>
+        </li>
+        <hr>
+        <li class="nav-item">
+          <a href="#" class="nav-link collapsed" id="CompanyManagementNav" data-target="ClientManagement">
+            <i class="bi bi-file-earmark-person"></i>
+            <span onclick="clearForms()">Klantbeheer</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link collapsed" id="UserManagementNav" data-target="UserManagement">
+            <i class="bi bi-person-gear"></i>
+            <span onclick="clearForms()">Gebruikerbeheer</span>
+          </a>
+        </li>
+        <hr>
+        <li class="nav-item">
+          <a href="#" class="nav-link collapsed" data-target="Settings">
+            <i class="bi bi-gear"></i>
+            <span>Instellingen</span>
+          </a>
+        </li>
+        <a href="http://localhost/regit/pages/logout.php" class="nav-link collapsed">
+          <li class="nav-item">
+            <i class="bi bi-box-arrow-in-right"></i>
+            <span>Uitloggen</span>
+          </li>
+        </a>
+
+      <?php
+    }
+      ?>
+
   </ul>
 
 </aside><!-- End Sidebar-->
